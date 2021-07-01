ceadad568f86e00b01685cac9f88e3b3a598274e
sandi <sandisunandar99@gmail.com>
2021-07-01 11:16:24 +0700

    fungsi multiple return

---
+++ fungsi_multiple_return/fungsi_multiple_return.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// ambil fungsi multiple return
+	var diameter float64 = 62
+	luas, keliling := FungsiMultipeReturun(diameter)
+
+	fmt.Printf("luas =  %f ", luas)
+	fmt.Printf("keliling =  %f ", keliling)
+
+	var NamaAnda = [2]string{"dian", "sastro"}
+
+	depan, belakang := SiapaNama(NamaAnda)
+	fmt.Println(depan)
+	fmt.Println(belakang)
+
+}
+
+func FungsiMultipeReturun(d float64) (float64, float64) {
+
+	// hitung luas
+	var luas = math.Pi * math.Pow(d/2, 2)
+
+	// keliling
+	var keliling = math.Pi * d
+
+	return luas, keliling
+}
+
+func SiapaNama(nama [2]string) (depan, belakang string) {
+	depan = nama[0]
+	belakang = nama[1]
+	return
+}
