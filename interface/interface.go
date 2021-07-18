308b9a55551271c61c09a520e94e45ceef6d3e8d
sandi sunandar <sandisunandar99@gmail.com>
2021-07-18 19:07:39 +0700

    interface

---
+++ interface/interface.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type hitung interface {
+	luas() float64
+	keliling() float64
+}
+
+type lingkaran struct {
+	diameter float64
+}
+
+func (l lingkaran) jariJari() float64 {
+	return l.diameter / 2
+}
+
+func (l lingkaran) luas() float64 {
+	return math.Pi * math.Pow(l.jariJari(), 2)
+}
+
+func (l lingkaran) keliling() float64 {
+	return math.Pi * l.diameter
+}
+
+func main() {
+	var hitungLingkaran hitung
+
+	hitungLingkaran = lingkaran{24.2}
+	fmt.Println("=== HITUNG LINGKARAN ===")
+	fmt.Println("Luas : ", hitungLingkaran.luas())
+	fmt.Println("Keliling :", hitungLingkaran.keliling())
+	fmt.Println("Jari-jari : ", hitungLingkaran.(lingkaran).jariJari())
+}
