6276623d2b2924e1a2e003684a79e4c33f28a06b
sandi sunandar <sandisunandar99@gmail.com>
2021-07-18 15:07:15 +0700

    fungsi variadic

--- fungsi_variadic/fungsi_variadic.go
+++ fungsi_variadic/fungsi_variadic.go
@@ -1,6 +1,28 @@
 package main
 
+import "fmt"
+
 // fungsi variadic biasanya ditandai dengan (...)
 func main() {
+	hasil := calc(1, 1, 1, 1, 1, 1, 1)
 
+	fmt.Printf("hasil =  %.0f \n", hasil)
+
+	ikan(3, "juaer", "gurame", "tomang")
+}
+
+// tanda variadic ialah pada fungsi terdapat multiple variabel dengan tipe data sama (...)
+func calc(n ...int) float64 {
+	var sum int = 0
+
+	for _, num := range n {
+		sum += num
+	}
+
+	return float64(sum)
+}
+
+func ikan(jumlah int, ikan ...string) {
+	fmt.Println(jumlah)
+	fmt.Println(ikan)
 }
