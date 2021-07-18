9cbe06c51556bd05ceffbd98285dac348892d5fc
sandi sunandar <sandisunandar99@gmail.com>
2021-07-18 15:31:45 +0700

    pointer

---
+++ pointer/pointer.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Variabel bertipe pointer ditandai dengan adanya tanda asterisk (*)
+
+func main() {
+	var numberA int = 4
+	var numberB *int = &numberA
+
+	fmt.Println("numberA (value)   :", numberA)
+	fmt.Println("numberA (address) :", &numberA)
+	fmt.Println("numberB (value)   :", *numberB)
+	fmt.Println("numberB (address) :", numberB)
+
+	fmt.Println("")
+
+	numberA = 5
+
+	fmt.Println("numberA (value)   :", numberA)
+	fmt.Println("numberA (address) :", &numberA)
+	fmt.Println("numberB (value)   :", *numberB)
+	fmt.Println("numberB (address) :", numberB)
+}
