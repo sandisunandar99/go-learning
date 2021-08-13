1acd5eb3b0390b077b91005e20849755bb9ea27b
sandi sunandar <sandisunandar99@gmail.com>
2021-08-13 10:50:24 +0700

    defer and exit

---
+++ defer_n_exit/defer_n_exit.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+/**
+	Defer digunakan untuk mengakhirkan eksekusi sebuah statement tepat sebelum blok fungsi selesai.
+	Exit digunakan untuk menghentikan program secara paksa (bener2 stack ga bisa dilanjutkan lagi)
+
+**/
+
+func main() {
+	// contohDefer()
+	contohExit()
+}
+
+func contohDefer() {
+	defer fmt.Println("awal")
+	fmt.Println("akhir")
+}
+
+func contohExit() {
+	fmt.Println("awal")
+	os.Exit(1) // dia akan behenti disini
+	fmt.Println("akhir")
+}
